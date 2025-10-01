301aaea5fe81062152fce7591cfd29a0168772a7
BobCatLunc <bobcatlunc@gmail.com>
2025-10-01 16:33:53 +0530

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,17 @@
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Uint128};
 use cw_storage_plus::Item;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
-pub const CW20_ADDRESS: Item<Addr> = Item::new("cw20_address");
-pub const ADMIN: Item<Addr> = Item::new("admin");
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub cw20_address: Addr,
+    pub admin: Addr,
+    pub total_uluna_burned: Uint128,
+    pub total_tokens_minted: Uint128,
+    pub burn_threshold: Uint128,
+    pub max_mint_ratio: Uint128,
+    pub paused: bool, // New field to track pause status
+}
+
+pub const CONFIG: Item<Config> = Item::new("config");
